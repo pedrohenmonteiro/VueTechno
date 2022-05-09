3253e2913bb8ceb328fccab48b2a339783c21b43
pedrohenmonteiro <pedromont436@gmail.com>
2022-05-08 21:29:01 -0300

    modal

--- app.js
+++ app.js
@@ -3,12 +3,33 @@ const vm = new Vue({
   data: {
     mensagem: "O Vue está funcionando",
     produtos: [],
+    produto: false,
+  },
+  filters: {
+    numberToPrice(valor) {
+      return valor.toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      });
+    },
   },
   methods: {
+    handleClick(e) {
+      if (e.target === e.currentTarget) {
+        this.produto = false;
+      }
+    },
+
     produtosAPI() {
       fetch("./api/produtos.json")
         .then((r) => r.json())
         .then((r) => (this.produtos = r));
+    },
+
+    produtoAPI(id) {
+      fetch(`./api/produtos/${id}/dados.json`)
+        .then((r) => r.json())
+        .then((r) => (this.produto = r));
     },
   },
   created() {
